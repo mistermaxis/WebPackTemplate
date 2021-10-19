31e7c67a046fff0d0516280d5e7886cd8c551b80
Maximiliano Diaz <87093942+mistermaxis@users.noreply.github.com>
2021-10-19 13:38:46 -0300

    Update webpack.config.js
    Add image loader to webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -26,16 +26,14 @@ module.exports = {
   module: {
 
     rules: [
-
       {
-
         test: /\.css$/i,
-
         use: ['style-loader', 'css-loader'],
-
       },
-
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
-
   },
 };
